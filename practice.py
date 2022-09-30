05a8fe6a586ff6c38de4712bfdf551a60b9340c9
Felix Vargas Jr <109393467+NeverGiveUp23@users.noreply.github.com>
2022-09-30 12:09:36 -0400

    Update practice.py

--- practice.py
+++ practice.py
@@ -6,3 +6,17 @@ if unit.upper() == "L":
 else:
     converted = weight / 0.45
     print(f" You are {converted} pounds")
+    
+    ## guessing game
+    
+    secret_number  = 5
+guess_number = 0
+guess_limit = 3
+while guess_number < guess_limit:
+    guess = int(input("Guess: "))
+    guess_number += 1
+    if guess == secret_number:
+        print("You Won")
+        break
+else:
+    print("Sorry you've failed")
